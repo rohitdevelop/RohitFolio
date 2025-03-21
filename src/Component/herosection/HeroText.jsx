1a3392e53bf52780b255c11602ab76cf202e8695
Rohit singh <rohitdev124421@gmail.com>
2025-03-21 15:37:52 +0530

    Updated Hero Section UI

--- src/Component/herosection/HeroText.jsx
+++ src/Component/herosection/HeroText.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const HeroText = () => {
   return (
@@ -19,6 +22,39 @@ const HeroText = () => {
         eager to build innovative web experiences and  
         share knowledge with others.
       </p>
+      <div className="flex flex-wrap justify-center md:justify-start gap-4 mt-6">
+  <a
+    href="https://www.linkedin.com/in/rohit-singh-b7777730a/"
+    className="text-white hover:text-red-500 transition duration-300"
+  >
+    <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
+  </a>
+  <a
+    href="https://github.com/rohitdevelop"
+    className="text-white hover:text-red-500 transition duration-300"
+  >
+    <FontAwesomeIcon icon={faGithub} className="text-2xl" />
+  </a>
+  <a
+    href="mailto:rohitdev124421@gmail.com"
+    className="text-white hover:text-red-500 transition duration-300"
+  >
+    <FontAwesomeIcon icon={faEnvelope} className="text-2xl" />
+  </a>
+  <a
+    href="https://x.com/Rohit_coder5"
+    className="text-white hover:text-red-500 transition duration-300"
+  >
+    <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
+  </a>
+  <a
+    href="https://www.instagram.com/rohit_coder5/?hl=en"
+    className="text-white hover:text-red-500 transition duration-300"
+  >
+    <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
+  </a>
+</div>
+
     </div>
   );
 };
